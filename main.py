935a67c005cda3913a94727c787455009c58723d
unknown <alonso.salas@usach.cl>
2021-11-29 22:04:30 -0300

    added message in image

--- main.py
+++ main.py
@@ -6,24 +6,46 @@ Created on Fri Oct 29 14:03:53 2021
 """
 
 from PIL import Image as img;
+from numpy import asarray
+import math
 
-#class image :
- #   def __init__ (self, rute):
- #       self.rute = rute
- #       self.imagen = img.open(rute)
- #   def img2data (self):
- #       data = asarray(self.imagen)
- #       return data
- #   def replace_data (self, data, message):
- #       new_data = data
- #       i= 0
- #       for pixel in new_data:
- #           for color in pixel:
- #               color = color + message[i]
-                
+class image :
+    def __init__ (self, route):
+        self.route = route
+        self.imagen = img.open(route)
+    def img2data (self):
+        data = asarray(self.imagen)
+        return data
+    def replace_data (self, data, message,binlist):
+        i=0
+        j=0
+        k=0
+        while i < len(binlist):
+            k=0
+            while k < 3 :
+                data[j][k] += binlist[i]
+                k +=1
+                i +=1
+            j+=1
+        return data
+    def Save_new_image(self,route):
+        new_image=img.fromarray(self.data)
+        new_image.save("moded.png")
+        return True
+    def extract_message(data, message_len):
+        binlist = []
+        for pixel in range(math.ceil(message_len/3)):
+            for color in pixel:
+                binlist.append(int(bin(color)[-1]))
+        return binlist
+        
+    
+        
+            
 class message  :
     def __init__(self,text):
         self.binlist=[]
+        self.bitlist=[]
         self.text=text
         self.tex2bin(self.text)  
     def tex2bin (self,text):
@@ -33,4 +55,7 @@ class message  :
             int_letter=int(hexa_letter, base=16)
             bin_letter=bin(int_letter)[2:]
             self.binlist.append(bin_letter)
+            self.bitlist=[int(bit) for byte in self.binlist for bit in byte]
+            
         return True
+
